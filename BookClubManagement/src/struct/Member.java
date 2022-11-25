8324bcfdbd7ec24fb5a2746b045d1ea995db45f2
zzo3ozz <hhzzo@DESKTOP-VCOTNUF>
2022-11-25 23:03:25 +0900

    비밀번호 변경 기능 구현

--- BookClubManagement/src/struct/Member.java
+++ BookClubManagement/src/struct/Member.java
@@ -24,6 +24,43 @@ public class Member {
 	}
 	
 	public void changePass(String nowPW, String newPW) {
+		Connection con = DBConnect.makeConnection();
+		PreparedStatement pstmt = null;
 		
+		try {// DB에 저장된 pw값 가져오기
+			String select = "select pw from Member where m_num = ?";
+
+			pstmt = con.prepareStatement(select);
+			pstmt.setInt(1, this.num);
+			ResultSet rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				String dbPW = rs.getString(1);
+				
+
+				if(nowPW.equals(dbPW)) {
+					String update = "update Member set pw = ? where m_num = ?";
+					
+					pstmt = con.prepareStatement(update);
+					pstmt.setString(1, newPW);
+					pstmt.setInt(2, this.num);
+					
+					int i = pstmt.executeUpdate();
+					
+					if (i == 1) {
+						System.out.println("갱신 성공!");
+					}
+				} else {
+					System.out.print("비밀번호가 일치하지 않습니다.");
+				}
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println("갱신 실패!");
+		}
+		
+		if(pstmt != null) try{ pstmt.close();} catch(SQLException e){};                   
+        if(con != null) try{ con.close();} catch(SQLException e){};
 	}
 }
