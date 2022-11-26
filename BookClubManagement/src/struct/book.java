9ca4a486dbaf529c6261586a1a0896a2c534de06
zzo3ozz <hhzzo@DESKTOP-VCOTNUF>
2022-11-26 15:14:52 +0900

    book class 생성

---
+++ BookClubManagement/src/struct/book.java
@@ -0,0 +1,14 @@
+package struct;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class book {
+	private int id;
+	String title;
+	ArrayList<String> author;
+	String genre;
+	String cover;
+	String owner;
+}
