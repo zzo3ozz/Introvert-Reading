16b1e9cf2bb08eec362e8b9c95ce7765a8560f54
zzo3ozz <hhzzo@DESKTOP-VCOTNUF>
2022-11-26 15:24:20 +0900

    login 기능 수정 및 기본 창 구성

--- BookClubManagement/src/LoginWin.java
+++ BookClubManagement/src/LoginWin.java
@@ -1,20 +1,59 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 
-public class LoginWin {
-	public JPanel loginPane = new JPanel();
+import function.LoginFunc;
+import function.Session;
+import struct.Member;
+
+public class LoginWin extends JPanel{
+	private JTextField idField;
+	private JPasswordField pwField;
+	private JLabel error;
 	
 	public LoginWin() {
-		loginPane.setBackground(Color.ORANGE);
-		loginPane.setBounds(700, 0, 300, 700);
-		loginPane.setLayout(null);
-		
-		JLabel idLa = new JLabel("ID"); idLa.setBounds(10, 250, 30, 20);
-		JTextField idField = new JTextField(); idField.setBounds(50, 250, 150, 20);
-		JLabel pwLa = new JLabel("PW"); pwLa.setBounds(10, 280, 30, 20);
-		JPasswordField pwField = new JPasswordField(); pwField.setBounds(50, 280, 150, 20);
+		setBackground(Color.ORANGE);
+		setBounds(700, 0, 300, 700);
+		setLayout(null);
 
-		loginPane.add(idLa); loginPane.add(pwLa);
-		loginPane.add(idField); loginPane.add(pwField);
+		JLabel idLa = new JLabel("ID");
+		idLa.setBounds(10, 250, 30, 20);
+		idField = new JTextField();
+		idField.setBounds(50, 250, 140, 20);
+		
+		JLabel pwLa = new JLabel("PW");
+		pwLa.setBounds(10, 280, 30, 20);
+		pwField = new JPasswordField();
+		pwField.setBounds(50, 280, 140, 20);
+		
+		JButton btn = new JButton("로그인");
+		btn.setBounds(200, 250, 80, 30);
+		error = new JLabel(); error.setBounds(50, 310, 250, 20);
+		error.setForeground(Color.RED);
+		
+		btn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String id = idField.getText();
+				String pw = String.valueOf(pwField.getPassword());
+				int state = LoginFunc.login(id, pw);
+				
+				if(state == LoginFunc.SUCCESS) {
+					setVisible(false);
+				} else if(state == LoginFunc.IDERROR) {
+					error.setText("등록된 아이디가 없습니다.");
+				} else if(state == LoginFunc.PWERROR) {
+					error.setText("비밀번호가 일치하지 않습니다.");
+				}
+			}
+		});
+
+		add(idLa);
+		add(pwLa);
+		add(idField);
+		add(pwField);
+		add(btn);
+		add(error);
 	}
 }
--- BookClubManagement/src/function/LoginFunc.java
+++ BookClubManagement/src/function/LoginFunc.java
@@ -4,7 +4,12 @@ import struct.Member;
 import java.sql.*;
 
 public class LoginFunc {
-	public static Member login(String id, String pw) {
+	public final static int SUCCESS = 1;
+	public final static int IDERROR = 0;
+	public final static int PWERROR = -1;
+	public final static int ERROR = -2;
+	
+	public static int login(String id, String pw) {
 		Connection con = DBConnect.makeConnection(); 
 		PreparedStatement pstmt = null;
 		
@@ -18,20 +23,23 @@ public class LoginFunc {
 				String dbPW = rs.getString(3);
 
 				if(dbPW.equals(pw)) {
-					// ID 존재, PW 일치할 경우 member return
+					// ID 존재, PW 일치할 경우
 					Member mem = new Member(rs);
 					
 					rs.close();
 					pstmt.close();
 					con.close();
 					
-					return mem;
-					
+					Session.login_member = mem;
+					Session.login_member.printInfo();
+					 
+					return SUCCESS;
 				}
 				System.out.println("비밀번호가 일치하지 않습니다.");
-				
+				return PWERROR;
 			} else {
 				System.out.print("아이디가 존재하지 않습니다.");
+				return IDERROR;
 			}
 		} catch(SQLException e) {
 			System.out.print("error!");
@@ -40,6 +48,6 @@ public class LoginFunc {
 		if(pstmt != null) try{ pstmt.close();} catch(SQLException e){};                   
         if(con != null) try{ con.close();} catch(SQLException e){};
         
-		return null;
+		return ERROR;
 	}
 }
