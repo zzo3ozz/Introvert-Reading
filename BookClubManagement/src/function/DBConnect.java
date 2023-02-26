964a0dff395fd030f46f479ba1f8db1803377ba1
Seoyoung <tjdud0814@naver.com>
2023-02-27 02:09:15 +0900

    Update DBConnect.java

--- BookClubManagement/src/function/DBConnect.java
+++ BookClubManagement/src/function/DBConnect.java
@@ -6,8 +6,8 @@ public class DBConnect {
 	// 노트북에서 작업 시 localhost로 변경할 것
 	public static Connection makeConnection() {
 		String url = "jdbc:mysql://localhost/bookclubdb?charecterEncoding=UTF-8&&serverTimezone=UT&useSSL=false";
-		String id = "BCAdmin";
-		String password = "Bc!753951";
+		String id = "******";
+		String password = "********";
 		Connection con = null;
 				
 		try {
